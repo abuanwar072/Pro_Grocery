c006d194b08fb7a19ab9cb2258db78755988b72e
Abu Anwar <abuanwar072@gmail.com>
2022-08-31 20:56:00 +0600

    Home page done

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -5,7 +5,6 @@ import 'package:grocery/core/constants/app_icons.dart';
 import '../../core/constants/app_defaults.dart';
 import '../../core/routes/app_routes.dart';
 import 'components/ad_space.dart';
-import 'components/header_home_page.dart';
 import 'components/our_new_item.dart';
 import 'components/popular_packs.dart';
 
@@ -14,53 +13,54 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return SafeArea(
-    //   child: SingleChildScrollView(
-    //     child: Column(
-    //       children: const [
-    //         HeaderHomePage(),
-    //         AdSpace(),
-    //         PopularPacks(),
-    //         SizedBox(height: AppDefaults.padding),
-    //         OurNewItem(),
-    //       ],
-    //     ),
-    //   ),
-    // );
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, AppRoutes.drawerPage);
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xFFF2F6F3),
-                  shape: const CircleBorder(),
-                ),
-                child: SvgPicture.asset(AppIcons.sidebarIcon),
-              ),
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 8, top: 4, bottom: 4),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 8),
                 child: ElevatedButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, AppRoutes.search);
+                    Navigator.pushNamed(context, AppRoutes.drawerPage);
                   },
                   style: ElevatedButton.styleFrom(
                     primary: const Color(0xFFF2F6F3),
                     shape: const CircleBorder(),
                   ),
-                  child: SvgPicture.asset(AppIcons.search),
+                  child: SvgPicture.asset(AppIcons.sidebarIcon),
                 ),
               ),
-            ],
-          ),
-        ],
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8, top: 4, bottom: 4),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, AppRoutes.search);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xFFF2F6F3),
+                      shape: const CircleBorder(),
+                    ),
+                    child: SvgPicture.asset(AppIcons.search),
+                  ),
+                ),
+              ],
+            ),
+            const SliverToBoxAdapter(
+              child: AdSpace(),
+            ),
+            const SliverToBoxAdapter(
+              child: PopularPacks(),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.symmetric(vertical: AppDefaults.padding),
+              sliver: const SliverToBoxAdapter(
+                child: OurNewItem(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
