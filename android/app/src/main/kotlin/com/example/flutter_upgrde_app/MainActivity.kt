ab92b5b438485d438f0543140900e7c4ae8822f2
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2025-10-26 19:04:59 -0500

    bug fixes & latest Flutter

--- android/app/src/main/kotlin/com/example/flutter_upgrde_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_upgrde_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/grocery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.grocery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
